2ddace159001bbba910a83e06900c2012f52d0f9
Anuj A <anujamirthalingam5@gmail.com>
2024-08-04 15:51:40 +0530

    Worked with What the forms

--- flask-app1.py
+++ flask-app1.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import data_required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "mahi@1234"
+
+
+class SimpleForm(FlaskForm):
+    name = StringField("What's your name", validators=[data_required()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -17,6 +26,16 @@ def findName(username):
 @app.errorhandler(404)
 def pageNotFound(e):
     return render_template("404.html"), 404
+
+
+@app.route('/name_form', methods = ['GET', "POST"])
+def name_for_form():
+    name = None
+    form = SimpleForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name_form.html", name=name, form=form)
 
 
 app.run(debug=True)
